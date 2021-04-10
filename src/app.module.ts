31e9abaa23e56f23cca4d45d2ac81dd256007eb1
deliveroon <cherif.benda@gmail.com>
2021-04-10 19:53:27 +0200

    Creation du middleware et mise en place d'un logger HTTP dans celui-ci

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod  } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MyLogger } from './middleware/logger.middleware'
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer){
+    consumer
+      .apply(MyLogger)
+      .forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
